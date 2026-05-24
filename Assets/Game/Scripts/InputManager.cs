7d8a74a623a07fa4b596f2ea315c0f1dd00787ac
Maya <mnurriskaputri@gmail.com>
2026-05-24 21:11:12 +0700

    finished first input manager

--- Assets/Game/Scripts/InputManager.cs
+++ Assets/Game/Scripts/InputManager.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using static GameInputAction;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
-public class InputManager : MonoBehaviour
+public class InputManager : MonoBehaviour, IPlayerActions
 {
     private GameInputAction _inputAction;
 
@@ -11,7 +13,28 @@ public class InputManager : MonoBehaviour
     {
         _inputAction = new GameInputAction();
         _inputAction.Enable();
+        _inputAction.Player.Enable(); //mengaktifkan action map Player
+        _inputAction.Player.SetCallbacks(this);
+    }
+    void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log(context.ReadValue<Vector2>());
+        }
+    }
+    void OnInteract(InputAction.CallbackContext context)
+    {
+        Debug.Log("Interact");
     }
 
-    
+    void IPlayerActions.OnMove(InputAction.CallbackContext context)
+    {
+        OnMove(context);
+    }
+
+    void IPlayerActions.OnInteract(InputAction.CallbackContext context)
+    {
+        OnInteract(context);
+    }
 }
